20fcdb80cc1fb8282cf77a892db18963a53d1496
katy-1307 <bibchuk@ukr.net>
2025-05-01 21:47:35 +0300

    api tests2

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -2,8 +2,18 @@ import requests
 
 class GitHub: 
 
-    def get_user_defunkt(self):
-        r = requests.get('https://api.github.com/users/defunkt')
+    def get_user(self, username):
+        r = requests.get(f'https://api.github.com/users/{username}')
         body = r.json()
     
+        return body
+    
+    def search_repo(self, name):
+        r = requests.get(
+            "https://api.github.com/search/repositories",
+            params = {"q": name}
+        )
+    
+        body = r.json()
+
         return body
--- tests/api/tert_github_api.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from modules.api.clients.github import GitHub
-
-@pytest.mark.api
-def test_user_exists():
-    api = GitHub
-    user = api.get_user_defunkt()
-    assert user['login'] == 'defunkt'
